5d5555bfe2249a3bbf32f8b92901abb2ed80d392
Vu Quang Huy <hacktyper12@gmail.com>
2026-09-14 09:04:47 +0700

    Add javascript file

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Git Basic Practice - Bài tập thực hành Git");
+
+function greetUser(name) {
+    return `Xin chào ${name}, chúc bạn thực hành Git hiệu quả!`;
+}
+
+console.log(greetUser("Học viên"));
